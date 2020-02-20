439983d8b6a898dd4864997aeb0e07357ae2a266
Robert Forsman <git@thoth.purplefrog.com>
2020-02-20 11:34:12 -0500

    test the min opcode

--- mips-compiler/src/tests.rs
+++ mips-compiler/src/tests.rs
@@ -516,6 +516,17 @@ pub fn test_max() -> Result<(), MultiError>
     check_binary_operator_019(&program, 3.0, -1.0, 3.0)
 }
 
+#[test]
+pub fn test_min() -> Result<(), MultiError>
+{
+    let source = include_str!("tests/test_min.mips");
+    let program = compile(source)?;
+
+    check_binary_operator_019(&program, 2.0, 2.5, 2.0)?;
+
+    check_binary_operator_019(&program, 3.0, -1.0, -1.0)
+}
+
 //
 
 #[test]
